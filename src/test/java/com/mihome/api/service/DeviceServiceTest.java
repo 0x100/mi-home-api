57aec361cf814109c974a781a9e9175eeb093e13
Ilya Lysenko <lysenko.ilya@gmail.com>
2020-01-09 17:08:31 +0300

    #10 Remove unused imports

--- src/test/java/com/mihome/api/service/DeviceServiceTest.java
+++ src/test/java/com/mihome/api/service/DeviceServiceTest.java
@@ -11,18 +11,11 @@ import org.junit.runners.JUnit4;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
